ffbab007e50998b4160c0dfd8641e70d4de9c349
Anton Heestand <anton.heestand@gmail.com>
2022-05-13 17:18:27 +0200

    3D

--- Sources/AsyncGraphics/Views/Graphic3DView.swift
+++ Sources/AsyncGraphics/Views/Graphic3DView.swift
@@ -9,16 +9,16 @@ import VoxelView
 public struct Graphic3DView: View {
     
     private let graphic3D: Graphic3D
-    private let zoom: CGFloat
+    private let scale: CGFloat
     private let rotationX: Angle
     private let rotationY: Angle
     
     public init(graphic3D: Graphic3D,
-                zoom: CGFloat = 1.0,
+                scale: CGFloat = 1.0,
                 rotationX: Angle = .zero,
                 rotationY: Angle = .zero) {
         self.graphic3D = graphic3D
-        self.zoom = zoom
+        self.scale = scale
         self.rotationX = rotationX
         self.rotationY = rotationY
     }
@@ -26,7 +26,7 @@ public struct Graphic3DView: View {
     public var body: some View {
         VoxelView(texture: graphic3D.texture,
                   textureID: graphic3D.id,
-                  zoom: zoom,
+                  scale: scale,
                   rotationX: rotationX,
                   rotationY: rotationY)
     }
