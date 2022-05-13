a7bdc9218cb9ed84c20ed6f6bc6a0e46e1bd827d
Anton Heestand <anton.heestand@gmail.com>
2022-05-13 12:54:30 +0200

    Orientation

--- Sources/AsyncGraphics/Views/Graphic3DView.swift
+++ Sources/AsyncGraphics/Views/Graphic3DView.swift
@@ -9,12 +9,25 @@ import VoxelView
 public struct Graphic3DView: View {
     
     private let graphic3D: Graphic3D
+    private let zoom: CGFloat
+    private let rotationX: Angle
+    private let rotationY: Angle
     
-    public init(graphic3D: Graphic3D) {
+    public init(graphic3D: Graphic3D,
+                zoom: CGFloat = 1.0,
+                rotationX: Angle = .zero,
+                rotationY: Angle = .zero) {
         self.graphic3D = graphic3D
+        self.zoom = zoom
+        self.rotationX = rotationX
+        self.rotationY = rotationY
     }
     
     public var body: some View {
-        VoxelView(texture: graphic3D.texture)
+        VoxelView(texture: graphic3D.texture,
+                  textureID: graphic3D.id,
+                  zoom: zoom,
+                  rotationX: rotationX,
+                  rotationY: rotationY)
     }
 }
