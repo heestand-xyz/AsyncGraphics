05e54166c786208d40b056f3c4cb82ccc0ac25c9
Anton Heestand <anton.heestand@gmail.com>
2022-08-17 18:40:34 +0200

    Mix

---
+++ Sources/AsyncGraphics/Graphics/Effects/Technical/Color Space/ColorSpace.swift
@@ -0,0 +1,31 @@
+//
+//  Created by Anton Heestand on 2022-04-02.
+//
+
+import Metal
+import TextureMap
+
+extension Graphic {
+    
+    public func convertColorSpace(from leadingColorSpace: TMColorSpace,
+                                  to trailingColorSpace: TMColorSpace) async throws -> Graphic {
+        
+        let texture = try await texture.convertColorSpace(from: leadingColorSpace.cgColorSpace,
+                                                          to: trailingColorSpace.cgColorSpace)
+        
+        return Graphic(name: "Color Space", texture: texture, bits: bits, colorSpace: trailingColorSpace)
+    }
+    
+    public func applyColorSpace(_ colorSpace: TMColorSpace) async throws -> Graphic {
+        
+        let texture = try await texture.convertColorSpace(from: self.colorSpace.cgColorSpace,
+                                                          to: colorSpace.cgColorSpace)
+        
+        return Graphic(name: "Color Space", texture: texture, bits: bits, colorSpace: colorSpace)
+    }
+    
+    public func assignColorSpace(_ colorSpace: TMColorSpace) -> Graphic {
+        
+        Graphic(name: "Color Space", texture: texture, bits: bits, colorSpace: colorSpace)
+    }
+}
