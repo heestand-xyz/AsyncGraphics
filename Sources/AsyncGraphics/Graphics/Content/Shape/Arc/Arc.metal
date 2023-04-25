cef0388a3b23315d24e48a82e29734f4942fe203
Anton Heestand <anton.heestand@gmail.com>
2023-04-25 13:34:54 +0900

    Arc

--- Sources/AsyncGraphics/Graphics/Content/Shape/Arc/Arc.metal
+++ Sources/AsyncGraphics/Graphics/Content/Shape/Arc/Arc.metal
@@ -1,128 +1,67 @@
-////
-////  Created by Anton Heestand on 2017-11-17.
-////  Copyright © 2017 Anton Heestand. All rights reserved.
-////
 //
-//#include <metal_stdlib>
-//using namespace metal;
+//  Created by Anton Heestand on 2017-11-17.
+//  Copyright © 2017 Anton Heestand. All rights reserved.
 //
-//struct VertexOut {
-//    float4 position [[position]];
-//    float2 texCoord;
-//};
-//
-//struct Uniforms{
-//    float s;
-//    float af;
-//    float at;
-//    float ao;
-//    float x;
-//    float y;
-//    float e;
-//    float ar;
-//    float ag;
-//    float ab;
-//    float aa;
-//    float er;
-//    float eg;
-//    float eb;
-//    float ea;
-//    float br;
-//    float bg;
-//    float bb;
-//    float ba;
-//    float premultiply;
-//    float resx;
-//    float resy;
-//    float aspect;
-//    float tile;
-//    float tileX;
-//    float tileY;
-//    float tileResX;
-//    float tileResY;
-//    float tileFraction;
-//};
-//
-//fragment float4 contentGeneratorArcPIX(VertexOut out [[stage_in]],
-//                                       const device Uniforms& in [[ buffer(0) ]],
-//                                       sampler s [[ sampler(0) ]]) {
-//    float pi = M_PI_F;
-//
-//    float u = out.texCoord[0];
-//    float v = out.texCoord[1];
-//    if (in.tile > 0.0) {
-//        u = (in.tileX / in.tileResX) + u * in.tileFraction;
-//        v = (in.tileY / in.tileResY) + v * in.tileFraction;
-//    }
-//    v = 1 - v; // Content Flip Fix
-//    float onePixel = 1.0 / max(in.resx, in.resy);
-//
-//    float4 ac = float4(in.ar, in.ag, in.ab, in.aa);
-//    float4 ec = float4(in.er, in.eg, in.eb, in.ea);
-//    float4 bc = float4(in.br, in.bg, in.bb, in.ba);
-//
-//    float4 c = bc;
-//
-//    float e = in.e;
-//    if (e < 0) {
-//        e = 0;
-//    }
-//
-//    float x = (u - 0.5) * in.aspect - in.x;
-//    float y = v - 0.5 - in.y;
-//
-//    float a = -atan2(y, x);
-//    float af = in.af * pi * 2 - pi / 2;
-//    float at = in.at * pi * 2 - pi / 2;
-//    float ao = in.ao * pi * 2;
-//    float afo = af + ao;
-//    if (afo < -pi) {
-//        afo = afo + floor((-afo + pi) / (pi * 2)) * (pi * 2);
-//    } else if (afo > pi) {
-//        afo = afo - floor((afo + pi) / (pi * 2)) * (pi * 2);
-//    }
-//    float ato = at + ao;
-//    if (ato < -pi) {
-//        ato = ato + floor((-ato + pi) / (pi * 2)) * (pi * 2);
-//    } else if (ato > pi) {
-//        ato = ato - floor((ato + pi) / (pi * 2)) * (pi * 2);
-//    }
-//
-//    if (afo == ato) {
-//        return bc;
-//    }
-//
-//    if (afo < ato ?
-//        a >= afo && a <= ato :
-//        !(a <= afo && a >= ato)) {
-//
-//        float dist = sqrt(pow(x, 2) + pow(y, 2));
-//
-//        if (e > 0.0) {
-//            if (dist < in.s - e / 2 - onePixel / 2) {
-//                c = ac;
-//            } else if (dist < in.s - e / 2 + onePixel / 2) {
-//                float fraction = (dist - (in.s - e / 2 - onePixel / 2)) / onePixel;
-//                c = ac * (1.0 - fraction) + ec * fraction;
-//            } else if (dist < in.s + e / 2 - onePixel / 2) {
-//                c = ec;
-//            } else if (dist < in.s + e / 2 + onePixel / 2) {
-//                float fraction = (dist - (in.s + e / 2 - onePixel / 2)) / onePixel;
-//                c = ec * (1.0 - fraction) + bc * fraction;
-//            }
-//        } else {
-//            if (dist < in.s - onePixel / 2) {
-//                c = ac;
-//            } else if (dist < in.s + onePixel / 2) {
-//                float fraction = (dist - (in.s - onePixel / 2)) / onePixel;
-//                c = ac * (1.0 - fraction) + bc * fraction;
-//            }
-//        }
-//    }
-//
-//    if (in.premultiply) {
-//        c = float4(c.r * c.a, c.g * c.a, c.b * c.a, c.a);
-//    }
-//
-//    return c;
-//}
+
+#include <metal_stdlib>
+using namespace metal;
+
+#import "../../../../Metal/Content/radius_header.metal"
+
+struct VertexOut {
+    float4 position [[position]];
+    float2 texCoord;
+};
+
+struct Uniforms {
+    bool premultiply;
+    bool antiAlias;
+    float angleCenter;
+    float angleLength;
+    float radius;
+    packed_float2 position;
+    float edgeRadius;
+    packed_float4 foregroundColor;
+    packed_float4 edgeColor;
+    packed_float4 backgroundColor;
+    packed_float2 resolution;
+};
+
+fragment float4 arc(VertexOut out [[stage_in]],
+                    const device Uniforms& uniforms [[ buffer(0) ]]) {
+    float pi = M_PI_F;
+    
+    float width = uniforms.resolution.x;
+    float height = uniforms.resolution.y;
+    float aspectRatio = width / height;
+
+    float u = out.texCoord[0];
+    float v = out.texCoord[1];
+    float onePixel = 1.0 / max(width, height);
+
+    float4 foregroundColor = uniforms.foregroundColor;
+    float4 edgeColor = uniforms.edgeColor;
+    float4 backgroundColor = uniforms.backgroundColor;
+    
+    float xRadius = (u - 0.5) * aspectRatio - uniforms.position.x;
+    float yRadius = (v - 0.5) - uniforms.position.y;
+    float radius = sqrt(pow(xRadius, 2) + pow(yRadius, 2));
+    
+    float currentAngle = atan2(yRadius, xRadius);
+    float angleCenter = uniforms.angleCenter * pi * 2;
+    float angleLength = uniforms.angleLength * pi * 2;
+    float startAngle = angleCenter - angleLength / 2;
+    float endAngle = angleCenter + angleLength / 2;
+    
+    float4 color = backgroundColor;
+    
+    if (startAngle != endAngle && currentAngle >= startAngle && currentAngle <= endAngle) {
+        color = radiusColor(radius, uniforms.radius, uniforms.edgeRadius, foregroundColor, edgeColor, backgroundColor, uniforms.antiAlias, uniforms.premultiply, onePixel);
+    }
+    
+    if (uniforms.premultiply) {
+        color = float4(color.rgb * color.a, color.a);
+    }
+    
+    return color;
+}
--- Sources/AsyncGraphics/Graphics/Content/Shape/Arc/Arc.swift
+++ Sources/AsyncGraphics/Graphics/Content/Shape/Arc/Arc.swift
@@ -1,93 +1,106 @@
-////
-////  Created by Anton Heestand on 2022-08-21.
-////
 //
-//import CoreGraphics
-//import CoreGraphicsExtensions
-//import PixelColor
+//  Created by Anton Heestand on 2022-08-21.
 //
-//extension Graphic {
-//
-//    private struct CircleUniforms {
-//        let premultiply: Bool
-//        let antiAlias: Bool
-//        let radius: Float
-//        let position: PointUniform
-//        let edgeRadius: Float
-//        let foregroundColor: ColorUniform
-//        let edgeColor: ColorUniform
-//        let backgroundColor: ColorUniform
-//        let resolution: SizeUniform
-//    }
-//
-//    public static func arc(radius: CGFloat,
-//                              center: CGPoint? = nil,
-//                              color: PixelColor = .white,
-//                              backgroundColor: PixelColor = .black,
-//                              resolution: CGSize,
-//                              options: ContentOptions = ContentOptions()) async throws -> Graphic {
-//
-//        let relativeRadius: CGFloat = radius / resolution.height
-//
-//        let center: CGPoint = center ?? (resolution.asPoint / 2)
-//        let relativePosition: CGPoint = (center - resolution / 2) / resolution.height
-//
-//        return try await Renderer.render(
-//            name: "Circle",
-//            shader: .name("circle"),
-//            uniforms: CircleUniforms(
-//                premultiply: options.premultiply,
-//                antiAlias: true,
-//                radius: Float(relativeRadius),
-//                position: relativePosition.uniform,
-//                edgeRadius: 0.0,
-//                foregroundColor: color.uniform,
-//                edgeColor: PixelColor.clear.uniform,
-//                backgroundColor: backgroundColor.uniform,
-//                resolution: resolution.uniform
-//            ),
-//            metadata: Renderer.Metadata(
-//                resolution: resolution,
-//                colorSpace: options.colorSpace,
-//                bits: options.bits
-//            )
-//        )
-//    }
-//
-//    public static func strokedArc(radius: CGFloat,
-//                                     center: CGPoint? = nil,
-//                                     lineWidth: CGFloat,
-//                                     color: PixelColor = .white,
-//                                     backgroundColor: PixelColor = .black,
-//                                     resolution: CGSize,
-//                                     options: ContentOptions = ContentOptions()) async throws -> Graphic {
-//
-//        let relativeRadius: CGFloat = radius / resolution.height
-//
-//        let center: CGPoint = center ?? (resolution / 2).asPoint
-//        let relativePosition: CGPoint = (center - resolution / 2) / resolution.height
-//
-//        let relativeLineWidth: CGFloat = lineWidth / resolution.height
-//
-//        return try await Renderer.render(
-//            name: "Circle",
-//            shader: .name("circle"),
-//            uniforms: CircleUniforms(
-//                premultiply: options.premultiply,
-//                antiAlias: true,
-//                radius: Float(relativeRadius),
-//                position: relativePosition.uniform,
-//                edgeRadius: Float(relativeLineWidth),
-//                foregroundColor: backgroundColor.uniform,
-//                edgeColor: color.uniform,
-//                backgroundColor: backgroundColor.uniform,
-//                resolution: resolution.uniform
-//            ),
-//            metadata: Renderer.Metadata(
-//                resolution: resolution,
-//                colorSpace: options.colorSpace,
-//                bits: options.bits
-//            )
-//        )
-//    }
-//}
+
+import CoreGraphics
+import CoreGraphicsExtensions
+import PixelColor
+import SwiftUI
+
+extension Graphic {
+
+    private struct ArcUniforms {
+        let premultiply: Bool
+        let antiAlias: Bool
+        let angleCenter: Float
+        let angleLength: Float
+        let radius: Float
+        let position: PointUniform
+        let edgeRadius: Float
+        let foregroundColor: ColorUniform
+        let edgeColor: ColorUniform
+        let backgroundColor: ColorUniform
+        let resolution: SizeUniform
+    }
+    
+    public static func arc(angle: Angle,
+                           length: Angle,
+                           radius: CGFloat? = nil,
+                           center: CGPoint? = nil,
+                           color: PixelColor = .white,
+                           backgroundColor: PixelColor = .black,
+                           resolution: CGSize,
+                           options: ContentOptions = ContentOptions()) async throws -> Graphic {
+
+        let radius: CGFloat = radius ?? min(resolution.width, resolution.height) / 2
+        let relativeRadius: CGFloat = radius / resolution.height
+
+        let center: CGPoint = center ?? (resolution.asPoint / 2)
+        let relativePosition: CGPoint = (center - resolution / 2) / resolution.height
+
+        return try await Renderer.render(
+            name: "Arc",
+            shader: .name("arc"),
+            uniforms: ArcUniforms(
+                premultiply: options.premultiply,
+                antiAlias: true,
+                angleCenter: angle.uniform,
+                angleLength: length.uniform,
+                radius: Float(relativeRadius),
+                position: relativePosition.uniform,
+                edgeRadius: 0.0,
+                foregroundColor: color.uniform,
+                edgeColor: PixelColor.clear.uniform,
+                backgroundColor: backgroundColor.uniform,
+                resolution: resolution.uniform
+            ),
+            metadata: Renderer.Metadata(
+                resolution: resolution,
+                colorSpace: options.colorSpace,
+                bits: options.bits
+            )
+        )
+    }
+
+    public static func strokedArc(angle: Angle,
+                                  length: Angle,
+                                  radius: CGFloat? = nil,
+                                  center: CGPoint? = nil,
+                                  lineWidth: CGFloat,
+                                  color: PixelColor = .white,
+                                  backgroundColor: PixelColor = .black,
+                                  resolution: CGSize,
+                                  options: ContentOptions = ContentOptions()) async throws -> Graphic {
+        
+        let radius: CGFloat = radius ?? min(resolution.width, resolution.height) / 2
+        let relativeRadius: CGFloat = radius / resolution.height
+
+        let center: CGPoint = center ?? (resolution / 2).asPoint
+        let relativePosition: CGPoint = (center - resolution / 2) / resolution.height
+
+        let relativeLineWidth: CGFloat = lineWidth / resolution.height
+
+        return try await Renderer.render(
+            name: "Arc",
+            shader: .name("arc"),
+            uniforms: ArcUniforms(
+                premultiply: options.premultiply,
+                antiAlias: true,
+                angleCenter: angle.uniform,
+                angleLength: length.uniform,
+                radius: Float(relativeRadius),
+                position: relativePosition.uniform,
+                edgeRadius: Float(relativeLineWidth),
+                foregroundColor: backgroundColor.uniform,
+                edgeColor: color.uniform,
+                backgroundColor: backgroundColor.uniform,
+                resolution: resolution.uniform
+            ),
+            metadata: Renderer.Metadata(
+                resolution: resolution,
+                colorSpace: options.colorSpace,
+                bits: options.bits
+            )
+        )
+    }
+}
