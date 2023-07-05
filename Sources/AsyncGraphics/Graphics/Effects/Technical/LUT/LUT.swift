5e2075b4fe9b3eea133a34ec21a4e727e28d1d45
Anton Heestand <anton.heestand@gmail.com>
2023-07-05 14:49:38 +0900

    Identity LUT

--- Sources/AsyncGraphics/Graphics/Effects/Technical/LUT/LUT.swift
+++ Sources/AsyncGraphics/Graphics/Effects/Technical/LUT/LUT.swift
@@ -1,4 +1,5 @@
 import Foundation
+import PixelColor
 
 extension Graphic {
     
@@ -57,9 +58,47 @@ extension Graphic {
     
     // MARK: Identity LUT
     
-    public static func identityLUT(resolution: CGSize) async throws -> Graphic {
-        var lut: Graphic = try await .color(.clear, resolution: resolution)
-        // ...
+    public static func identityLUT() async throws -> Graphic {
+
+        let count = 8
+        let powerCount = count * count
+
+        let partResolution = CGSize(width: powerCount,
+                                    height: powerCount)
+        let fullResolution = CGSize(width: powerCount * count,
+                                    height: powerCount * count)
+
+        let redGradient: Graphic = try await .gradient(direction: .horizontal, stops: [
+            GradientStop(at: 0.0, color: .black),
+            GradientStop(at: 1.0, color: .red),
+        ], resolution: partResolution)
+        let greenGradient: Graphic = try await .gradient(direction: .vertical, stops: [
+            GradientStop(at: 0.0, color: .black),
+            GradientStop(at: 1.0, color: .green),
+        ], resolution: partResolution)
+        let redGreenGradient: Graphic = try await redGradient + greenGradient
+
+        var lut: Graphic = try await .color(.black, resolution: fullResolution)
+        
+        for y in 0..<count {
+            let yFraction = CGFloat(y) / CGFloat(count - 1)
+            for x in 0..<count {
+                let xFraction = CGFloat(x) / CGFloat(count - 1)
+                let i = y * count + x
+                let fraction = CGFloat(i) / CGFloat(powerCount - 1)
+                
+                let blueColor = PixelColor(red: 0.0, green: 0.0, blue: fraction)
+                let blueSolid: Graphic = try await .color(blueColor, resolution: partResolution)
+                let part: Graphic = try await redGreenGradient + blueSolid
+                
+                let offset = CGPoint(
+                    x: (xFraction - 0.5) * (fullResolution.width - partResolution.width),
+                    y: (yFraction - 0.5) * (fullResolution.height - partResolution.height))
+                
+                lut = try await lut.transformBlended(with: part, blendingMode: .over, placement: .center, translation: offset)
+            }
+        }
+        
         return lut
     }
 }
