4c290821e30af5cffc9535fb52b6be5fb1b481b3
Anton Heestand <antonnorman.heestand@rakuten.com>
2023-11-22 17:05:32 +0900

    Tests

--- Tests/AsyncGraphicsTests/CodableGraphicsTests.swift
+++ Tests/AsyncGraphicsTests/CodableGraphicsTests.swift
@@ -1,38 +1,40 @@
-//import XCTest
-//@testable import AsyncGraphics
-//
-//final class CodableGraphicsTests: XCTestCase {
-//    
-//    let resolution = CGSize(width: 1000, height: 1000)
-//    let resolution3D = SIMD3<Int>(100, 100, 100)
-//    
-//    func testType() async throws {
-//        
-//        for type in CodableGraphicType.allCases {
-//            let instance: CodableGraphic = type.instance()
-//            XCTAssertEqual(type, instance.type)
-//        }
-//        
-//        for type in CodableGraphic3DType.allCases {
-//            let instance: CodableGraphic3D = type.instance()
-//            XCTAssertEqual(type, instance.type)
-//        }
-//    }
-//    
-//    func testRender() async throws {
-//        
-//        for type in CodableGraphicType.allCases {
-//            let instance: CodableGraphic = type.instance()
-//            if let content = instance as? ContentGraphic {
-//                _ = try await content.render(at: resolution, options: [])
-//            }
-//        }
-//        
-//        for type in CodableGraphic3DType.allCases {
-//            let instance: CodableGraphic3D = type.instance()
-//            if let content = instance as? ContentGraphic3D {
-//                _ = try await content.render(at: resolution3D, options: [])
-//            }
-//        }
-//    }
-//}
+import XCTest
+@testable import AsyncGraphics
+
+final class CodableGraphicsTests: XCTestCase {
+    
+    let resolution = CGSize(width: 1000, height: 1000)
+    let resolution3D = SIMD3<Int>(100, 100, 100)
+    
+    func testType() async throws {
+        
+        for type in CodableGraphicType.allCases {
+            let instance: CodableGraphicProtocol = type.instance()
+            print("------> 2D:", type)
+            XCTAssertEqual(type, instance.type)
+        }
+        
+        for type in CodableGraphic3DType.allCases {
+            let instance: CodableGraphic3DProtocol = type.instance()
+            print("------> 3D:", type)
+            XCTAssertEqual(type, instance.type)
+        }
+    }
+    
+    func testRender() async throws {
+        
+        for type in CodableGraphicType.allCases {
+            let instance: CodableGraphicProtocol = type.instance()
+            if let content = instance as? ContentGraphicProtocol {
+                _ = try await content.render(at: resolution, options: [])
+            }
+        }
+        
+        for type in CodableGraphic3DType.allCases {
+            let instance: CodableGraphic3DProtocol = type.instance()
+            if let content = instance as? ContentGraphic3DProtocol {
+                _ = try await content.render(at: resolution3D, options: [])
+            }
+        }
+    }
+}
