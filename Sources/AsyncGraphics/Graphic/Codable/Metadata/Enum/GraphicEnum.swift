0184c816d233267506a6af529ad2fc0c7008bd13
Anton Heestand <anton.heestand@gmail.com>
2024-01-04 22:17:18 +0900

    Blend and Stack view switcher

--- Sources/AsyncGraphics/Graphic/Codable/Metadata/Enum/GraphicEnum.swift
+++ Sources/AsyncGraphics/Graphic/Codable/Metadata/Enum/GraphicEnum.swift
@@ -1,5 +1,5 @@
 
-public protocol GraphicEnum: RawRepresentable, Codable, CaseIterable, Identifiable {
+public protocol GraphicEnum: RawRepresentable, Codable, CaseIterable, Identifiable, Hashable {
     var name: String { get }
     var index: UInt32 { get }
 }
