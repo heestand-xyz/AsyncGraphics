f71c7954a9467d99713230c5e7ab6fab0906b0b0
Anton Heestand <anton.heestand@gmail.com>
2025-02-17 19:30:06 +0900

    Pixel Color Data Node

--- Sources/AsyncGraphics/Graphics/Effects/Convert/Sample/Sample.swift
+++ Sources/AsyncGraphics/Graphics/Effects/Convert/Sample/Sample.swift
@@ -5,23 +5,57 @@
 //  Created by a-heestand on 2025/01/03.
 //
 
+import Foundation
 import CoreGraphics
 import CoreGraphicsExtensions
 import PixelColor
 
 extension Graphic {
+    
+    enum SubPixelError: LocalizedError {
+        case pixelLocationOutOfBounds
+        var errorDescription: String? {
+            switch self {
+            case .pixelLocationOutOfBounds:
+                "Sub pixel location is out of bounds."
+            }
+        }
+    }
+
+    /// Sample at a `location` coordinate, if the coordinate is fractional nearest pixels will be interpolated.
+    ///
+    /// Origin is at top left.
+    public func subPixel(at location: CGPoint) async throws -> PixelColor {
+        try await subPixel(x: location.x, y: location.y)
+    }
+    
+    /// Sample at an `x` and `y` coordinate, if the coordinate is fractional nearest pixels will be interpolated.
+    ///
+    /// Origin is at top left.
+    public func subPixel(x: CGFloat, y: CGFloat) async throws -> PixelColor {
+        try await subPixel(u: x / (resolution.width - 1.0),
+                           v: y / (resolution.height - 1.0))
+    }
 
     /// Sample at relative `uv` coordinates
     ///
-    /// `u` is horizontal, `v` is vertical, from top left, between `0.0` and `1.0`
+    /// `u` is horizontal, `v` is vertical, between `0.0` and `1.0`.
+    ///
+    /// Origin is at top left.
     public func subPixel(u: CGFloat, v: CGFloat) async throws -> PixelColor {
-        let width = Int(resolution.width)
-        let height = Int(resolution.height)
-        let uv = CGPoint(x: min(1.0, max(0.0, u)),
-                         y: min(1.0, max(0.0, v)))
-        let location: CGPoint = uv * resolution
+        guard u >= 0.0, u <= 1.0, v >= 0.0, v <= 1.0 else {
+            throw SubPixelError.pixelLocationOutOfBounds
+        }
+        let uv = CGPoint(x: u, y: v)
+        let location: CGPoint = uv * (resolution - 1.0) + 0.5
         let x = Int(location.x)
         let y = Int(location.y)
+        if CGFloat(x) == location.x - 0.5,
+           CGFloat(y) == location.y - 0.5 {
+            return try await pixel(x: x, y: y)
+        }
+        let width = Int(resolution.width)
+        let height = Int(resolution.height)
         let subPixelOffset: CGPoint = location - CGPoint(x: x, y: y)
         let topLeftPixelColor: PixelColor = try await pixel(x: x, y: y)
         let topRightPixelColor: PixelColor = x + 1 < width ? try await pixel(x: x + 1, y: y) : topLeftPixelColor
