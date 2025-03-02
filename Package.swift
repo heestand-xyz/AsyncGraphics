6d68c2148ae4e9df12c5ef606fe4894cad7c476c
Anton Heestand <anton.heestand@gmail.com>
2025-03-02 18:13:35 +0900

    Syntax Version

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .package(url: "https://github.com/heestand-xyz/SpatialExtensions", from: "1.0.0"),
         .package(url: "https://github.com/heestand-xyz/VideoFrames", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
-        .package(url: "https://github.com/apple/swift-syntax", from: "509.0.0"),
+        .package(url: "https://github.com/apple/swift-syntax", from: "510.0.0"),
     ],
     targets: [
         .target(
