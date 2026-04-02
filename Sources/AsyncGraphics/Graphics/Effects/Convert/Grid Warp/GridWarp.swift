d07cafe4f8945991080ed9d2d0cf74bfc5cc0959
Anton Heestand <anton.heestand@gmail.com>
2026-04-02 18:00:55 +0900

    Grid Warp

--- Sources/AsyncGraphics/Graphics/Effects/Convert/Grid Warp/GridWarp.swift
+++ Sources/AsyncGraphics/Graphics/Effects/Convert/Grid Warp/GridWarp.swift
@@ -18,7 +18,7 @@ extension Graphic {
             public var u: CGFloat
             public var v: CGFloat
             var rendererVertex: Renderer.Vertex {
-                Renderer.Vertex(x: x, y: y, u: u, v: v)
+                Renderer.Vertex(x: x * 2.0 - 1.0, y: (1.0 - y) * 2.0 - 1.0, u: u, v: v)
             }
             public init(uv: CGPoint) {
                 self.init(at: uv, uv: uv)
@@ -29,8 +29,16 @@ extension Graphic {
                 u = uv.x
                 v = uv.y
             }
+            init(x: CGFloat, y: CGFloat, u: CGFloat, v: CGFloat) {
+                self.x = x
+                self.y = y
+                self.u = u
+                self.v = v
+            }
         }
+        /// A list of rows of vertices.
         public var vertices: [[Vertex]] = []
+        /// Initialize with a list of rows of vertices.
         public init(vertices: [[Vertex]]) {
             self.vertices = vertices
         }
@@ -47,7 +55,7 @@ extension Graphic {
                 vertices.append(vertexRow)
             }
         }
-        /// Vertices for polygons. Grouped by 3 vertices.
+        /// Vertices for polygons. Grouped by 3 vertices per polygon.
         public var flatVertices: [[Vertex]] {
             var flatVertices: [[Vertex]] = []
             for (y, vertexRow) in vertices.enumerated() {
@@ -60,13 +68,29 @@ extension Graphic {
                     let bottomVertex = nextVertexRow[x]
                     guard nextVertexRow.indices.contains(x + 1) else { continue }
                     let bottomRightVertex = nextVertexRow[x + 1]
+                    let centerVetex = Vertex(
+                        x: (vertex.x + rightVertex.x + bottomVertex.x + bottomRightVertex.x) / 4,
+                        y: (vertex.y + rightVertex.y + bottomVertex.y + bottomRightVertex.y) / 4,
+                        u: (vertex.u + rightVertex.u + bottomVertex.u + bottomRightVertex.u) / 4,
+                        v: (vertex.v + rightVertex.v + bottomVertex.v + bottomRightVertex.v) / 4,
+                    )
                     flatVertices.append([
-                        vertex,
-                        rightVertex,
+                        centerVetex,
                         bottomVertex,
+                        vertex,
                     ])
                     flatVertices.append([
+                        centerVetex,
+                        vertex,
                         rightVertex,
+                    ])
+                    flatVertices.append([
+                        centerVetex,
+                        rightVertex,
+                        bottomRightVertex,
+                    ])
+                    flatVertices.append([
+                        centerVetex,
                         bottomRightVertex,
                         bottomVertex,
                     ])
@@ -76,10 +100,12 @@ extension Graphic {
         }
     }
     
+    /// Warp graphic with a grid.
     public func warp(grid: Grid) async throws -> Graphic {
         try await Renderer.render(
             name: "Grid Warp",
             shader: .passthrough,
+            graphics: [self],
             vertices: .direct(
                 grid.flatVertices.flatMap({ $0 }).map(\.rendererVertex),
                 type: .triangle
